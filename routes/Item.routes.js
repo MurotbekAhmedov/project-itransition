81364f7ee2a974ad4ac36b5b9b6e530c83ab4d8c
Murotbek <murotahmedoc@gmail.com>
2023-07-22 14:10:50 +0500

    fix collections-2

--- routes/Item.routes.js
+++ routes/Item.routes.js
@@ -1,39 +1,39 @@
-const {Router} = require('express')
-const config = require('config')
-const auth = require('../middleware/auth.middleware')
-const router = Router()
+// const {Router} = require('express')
+// const config = require('config')
+// const auth = require('../middleware/auth.middleware')
+// const router = Router()
 
-router.post('/generate', auth, async (req, res) => {
-  try {
-  
-    const link = new Link({
-      code, to, from, owner: req.user.userId
-    })
+// router.post('/generate', auth, async (req, res) => {
+//   try {
 
-    await link.save()
+//     const link = new Link({
+//       code, to, from, owner: req.user.userId
+//     })
 
-    res.status(201).json({ link })
-  } catch (e) {
-    res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
-  }
-})
+//     await link.save()
 
-router.get('/', auth, async (req, res) => {
-  try {
-    const links = await Link.find({ owner: req.user.userId })
-    res.json(links)
-  } catch (e) {
-    res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
-  }
-})
+//     res.status(201).json({ link })
+//   } catch (e) {
+//     res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
+//   }
+// })
 
-router.get('/:id', auth, async (req, res) => {
-  try {
-    const link = await Link.findById(req.params.id)
-    res.json(link)
-  } catch (e) {
-    res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
-  }
-})
+// router.get('/', auth, async (req, res) => {
+//   try {
+//     const links = await Link.find({ owner: req.user.userId })
+//     res.json(links)
+//   } catch (e) {
+//     res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
+//   }
+// })
 
-module.exports = router
+// router.get('/:id', auth, async (req, res) => {
+//   try {
+//     const link = await Link.findById(req.params.id)
+//     res.json(link)
+//   } catch (e) {
+//     res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
+//   }
+// })
+
+// module.exports = router
